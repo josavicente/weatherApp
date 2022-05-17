75f59bd7fd7d6150ec5a0a4f16172b6c7cd1e204
Josa Vicente <jvicenpe@gmail.com>
2022-05-17 21:19:13 +0200

    Initial Commit

---
+++ weatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weatherApp
+//
+//  Created by Josafat Vicente Pérez on 17/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Josafat Vicente Pérez on 17/5/22.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
